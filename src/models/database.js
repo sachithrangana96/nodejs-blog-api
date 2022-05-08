5911416e0ce93164b843a5fdf513ef8ff1f8de5f
sachithrangana96 <sachithrangana96@gmail.com>
2022-05-08 13:17:18 +0530

    test

--- src/models/database.js
+++ src/models/database.js
@@ -7,7 +7,7 @@ const POOL = mysql.createPool({
     localhost:process.env.RDS_HOSTNAME,
     user:process.env.RDS_USERNAME,
     password:process.env.RDS_PASSWORD,
-    database:process.env.RDS_PORT
+    database:process.env.RDS_DB_NAME
 })
 
 class DB {
